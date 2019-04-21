fc11656925080ab3cf1a1e79b2744433502f57ba
Kenechi Dukor <kennydukor@gmail.com>
2019-04-21 21:57:38 +0100

    react course

---
+++ REACT PRACTICE/JavaScript Review/javaS.js
@@ -0,0 +1,17 @@
+var humanBeing = {
+    Eye: 2,
+    Ear: "two",
+    Mouth: true
+};
+
+
+var Car = {};
+
+Car.Wheel = 4;
+Car.Color = "red";
+Car.Engine = true;
+
+var Cup = {};
+
+Cup["handle"] = 1;
+Cup["color"] = "red";
